7fcba325a90fdfc52c4abfca6db4abca3dab0db9
Stephen Tallowitz <stephen.tallowitz@petzi.ch>
2020-03-14 23:24:35 +0100

    Add colour to output

--- test/test.sh
+++ test/test.sh
@@ -1,27 +1,37 @@
 #!/bin/sh
 
-# Most tests require the daemon to be running.
-# Wait for 1 second for the daemon to start.
-/usr/sbin/slapd -u ldap -g ldap -F /etc/openldap/slapd.d -d 256 &
-sleep 1
+# Colour definitions
+CRes='\e[0m'
+CCya='\e[0;36m'
+CGre='\e[0;32m'
+CRed='\e[0;31m'
 
+# Function to test return codes of commands
 testReturnCode() {
 	testname="ReturnCodeTest"
 	name="$1"
 	expected="$2"
 	cmd="$3"
-	echo "[$0] [$testname] [$name] Starting - command: $cmd"
+	# shellcheck disable=SC2039
+	echo -e "$CCya [$0] [$testname] [$name] Starting $CRes"
 	# The following has to be unquoted due to very sensitive expansion
 	# shellcheck disable=SC2086
 	$cmd
 	rc="$?"
 	if [ "$rc" -ne "$expected" ]; then
-		echo "[$0] [$testname] [$name] FAILED. Expected: $expected, Actual: $rc"
+		# shellcheck disable=SC2039
+		echo -e "$CCya [$0] [$testname] [$name] $CRed FAILED. Expected: $expected, Actual: $rc $CRes"
 		exit 1
 	else
-		echo "[$0] [$testname] [$name] SUCCEEDED"
+		# shellcheck disable=SC2039
+		echo -e "$CCya [$0] [$testname] [$name] $CGre SUCCEEDED $CRes"
 	fi
 }
+
+# Most tests require the daemon to be running.
+# Wait for 1 second for the daemon to start.
+/usr/sbin/slapd -u ldap -g ldap -F /etc/openldap/slapd.d -d 256 &
+sleep 1
 
 cmd="slaptest"
 testReturnCode "slaptest configuration check" 0 "$cmd"
