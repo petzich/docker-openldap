595475f8422dcba89b90eda6c987012245895eea
Stephen Tallowitz <stephen.tallowitz@petzi.ch>
2020-03-15 19:10:22 +0100

    Remove ldapadd and process all files with ldapmodify

--- dist/bin/ldif.sh
+++ dist/bin/ldif.sh
@@ -8,12 +8,8 @@ log "Called with parameters: $*"
 
 find_ldif_files() {
 	all_files=$(find /setup -name "*.ldif" | sort)
-	add_files=$(echo "${all_files}" | grep "add.ldif")
-	mod_files=$(echo "${all_files}" | grep "mod.ldif")
-	conf_add_files=$(echo "${add_files}" | grep "/setup/conf")
-	conf_mod_files=$(echo "${mod_files}" | grep "/setup/conf")
-	rootdn_add_files=$(echo "${add_files}" | grep "/setup/rootdn")
-	rootdn_mod_files=$(echo "${mod_files}" | grep "/setup/rootdn")
+	conf_files=$(echo "${all_files}" | grep "/setup/conf")
+	rootdn_files=$(echo "${all_files}" | grep "/setup/rootdn")
 }
 
 replace_env_in_ldif() {
@@ -31,28 +27,10 @@ replace_env_in_all_ldif() {
 	done
 }
 
-conf_add() {
-	file="$1"
-	if [ -f "$file" ]; then
-		ldapadd -Q -Y EXTERNAL -H ldapi://%2Fvar%2Frun%2Fopenldap%2Fldapi -f "$file"
-	else
-		log "$file is not a file. not processing"
-	fi
-}
-
 conf_mod() {
 	file="$1"
 	if [ -f "$file" ]; then
 		ldapmodify -Q -Y EXTERNAL -H ldapi://%2Fvar%2Frun%2Fopenldap%2Fldapi -f "$file"
-	else
-		log "$file is not a file. not processing"
-	fi
-}
-
-rootdn_add() {
-	file="$1"
-	if [ -f "$file" ]; then
-		ldapadd -x -D "cn=manager,${SLAPD_ROOTDN}" -w "${SLAPD_ROOTPW}" -f "$file"
 	else
 		log "$file is not a file. not processing"
 	fi
@@ -72,22 +50,14 @@ if [ "$1" = "replace_env" ]; then
 	replace_env_in_all_ldif
 elif [ "$1" = "conf" ]; then
 	find_ldif_files
-	log "Processing conf_add_files"
-	for file in ${conf_add_files}; do
-		conf_add "$file"
-	done
-	log "Processing conf_mod_files"
-	for file in ${conf_mod_files}; do
+	log "Processing conf_files"
+	for file in ${conf_files}; do
 		conf_mod "$file"
 	done
 elif [ "$1" = "ldif" ]; then
 	find_ldif_files
-	log "Processing rootdn_add_files"
-	for file in ${rootdn_add_files}; do
-		rootdn_add "$file"
-	done
-	log "Processing rootdn_mod_files"
-	for file in ${rootdn_mod_files}; do
+	log "Processing rootdn_files"
+	for file in ${rootdn_files}; do
 		rootdn_mod "$file"
 	done
 else
